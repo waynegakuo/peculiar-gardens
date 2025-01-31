bea9359f6d07868214743a3c7a00e40c4a35fe82
Wayne Gakuo <waynejotham@gmail.com>
2025-02-01 00:02:22 +0300

    feat(project): set up firebase: hosting setup, firestore setup, GitHub actions and deployments setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import * as utils from '../app/core/utils';
+
+export const environment = {
+  production: false,
+  firebaseConfig: utils.firebaseConfig
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import * as utils from '../app/core/utils';
+
+export const environment = {
+  production: true,
+  firebaseConfig: utils.firebaseConfig
+};
