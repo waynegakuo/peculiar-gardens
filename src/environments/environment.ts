02d615cf647c367a36ccc3c8048ef132c499e044
Wayne Gakuo <waynejotham@gmail.com>
2025-02-04 07:25:44 +0300

    feat(project): set up firebase: firebase config details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
-import * as utils from '../app/core/utils';
-
 export const environment = {
   production: true,
-  firebaseConfig: utils.firebaseConfig
+  firebaseConfig: {
+    apiKey: "AIzaSyC95PHl5bCOxJW003Nv0_26Q8OBxqbCDns",
+    authDomain: "peculiar-gardens.firebaseapp.com",
+    projectId: "peculiar-gardens",
+    storageBucket: "peculiar-gardens.firebasestorage.app",
+    messagingSenderId: "156595131273",
+    appId: "1:156595131273:web:f72a5ea75a3694b9a3c758",
+    measurementId: "G-6CCBEZZY6B"
+  }
 };
