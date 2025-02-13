bfab1641751413c28aaa1b5e757bb7b61be17ab9
Fred Kisingo <fredrickkisingo@gmail.com>
2025-02-13 16:37:41 +0300

    feat(project): improve product card UI, use mock data to render products in browse section, create model for product, test use of signals to keep track of product cart count

--- src/app/shared/components/product/product.component.ts
+++ src/app/shared/components/product/product.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, computed, Input, signal} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
+import {Product} from '../../../models/product.model';
 
 @Component({
     selector: 'app-product',
@@ -13,5 +14,12 @@ import {MatIcon} from '@angular/material/icon';
     styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+  @Input() productData!: Product;
 
+  cartCount = signal(0);
+  productCount = computed(() => this.cartCount());
+
+  addToCart() {
+    this.cartCount.update((count) => count + 1);
+  }
 }
